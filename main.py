3edd889cbcbb5230a045722101187fa66ac8574f
Frederick Balderrama <antoniobalderramamendoza@gmail.com>
2025-05-04 21:47:17 -0400

    Change of header depending on color selection or eraser selection is already working, also the color being displayed for the rect or circle is being properly displayed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2
 import os
 import HandTracker as ht
-from utils import only
+from utils import only, isInRange
 
 header_folder_path = "Assets/Headers"
 header_files = os.listdir(header_folder_path)
@@ -17,6 +17,8 @@ current_header = headers[0]
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
+
+current_color = None
 
 if not cap.isOpened():
     print("Error: Could not open video.")
@@ -43,11 +45,31 @@ while True:
 
     fingers = tracker.fingersUp()
 
-    if only(fingers, [1, 2]):
-        print("Selection mode")
-    elif only(fingers, [1]):
-        print("Draw mode")
+    if only(fingers, [1, 2]): # Selection mode
+        if y1 < 125: 
+            if isInRange(x1, 188.75, 303.45):
+                current_header = headers[1]
+                current_color = (0, 0, 255)
+            elif isInRange(x1, 406.25, 520.95):
+                current_header = headers[2]
+                current_color = (255, 0, 0)
+            elif isInRange(x1, 623.75, 738.45):
+                current_header = headers[3]
+                current_color = (0, 0, 0)
+            elif isInRange(x1, 828.95, 966.25):
+                current_header = headers[4]
+                current_color = None
+
+        if current_color:
+            cv2.rectangle(frame, (x1, y1-30), (x2, y2+30), current_color, cv2.FILLED)
+        
+    elif only(fingers, [1]): # Draw mode
+        if current_color:
+            cv2.circle(frame, (x1, y1), 15, current_color, cv2.FILLED)
 
     frame[0:125, 0:1280] = current_header
     cv2.imshow("Image", frame)
     cv2.waitKey(1)
+
+cap.release()
+cv2.destroyAllWindows()
--- utils.py
+++ utils.py
@@ -5,4 +5,6 @@ def only(array, indexes):
         ref.add(array[i])
 
     return all(ref) and not any([x for i,x in enumerate(array) if i not in indexes])
-    
+
+def isInRange(value, min_value, max_value):
+    return min_value <= value <= max_value
